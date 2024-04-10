84d065e73b65c0de225c63b509bf4c5edf4b1600
lgesuellip <lgesuellipinto@uade.edu.ar>
2024-04-10 18:14:20 -0300

    Solver :brain:

--- core/tasks.py
+++ core/tasks.py
@@ -8,13 +8,14 @@ def planner_task() -> PromptTemplate:
     """
     return PromptTemplate(
         template=dedent("""
-            For the following tasks, make plans that can solve the problem step-by-step. For each plan,
-            indicate which external tool together with tool input to retrieve evidence. You can store the
-            evidence into a variable #E that can be called by later tools. (Plan : , #E1, Plan : , #E2, Plan : , ...)
+            For the following task, make plans that can solve the problem step-by-step. For each plan, \
+            indicate which external tool together with tool input to retrieve evidence. You can store the \
+            evidence into a variable #E that can be called by later tools. (Plan: , #E1, Plan: , #E2, Plan : , E3, ...)
 
             Tools can be one of the following:
             {exemplars}
 
+            For example:
             {few_shot}
 
             Begin!
@@ -23,4 +24,24 @@ def planner_task() -> PromptTemplate:
             Task: {query}
             """),
         input_variables=["exemplars", "few_shot", "query"],
+    )
+
+def solver_task() -> PromptTemplate:
+    """"
+    """
+    return PromptTemplate(
+        template=dedent("""
+            Solve the following task or problem. To solve the problem, we have made step-by-step Plan and \
+            retrieved corresponding Evidence to each Plan. Use them with caution since long evidence might \
+            contain irrelevant information.
+
+            {context}
+
+            Now solve the question or task according to provided Evidence above. Respond with the answer \
+            directly with no extra words.
+
+            Task: {query}
+            Response:
+            """),
+        input_variables=["query", "context"],
     )
